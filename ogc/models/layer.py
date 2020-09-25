57f11bbb78a634e66252da80ed6adc1334093046
Theo Kleinendorst <theo.kleinendorst@acaciawater.com>
2020-09-25 09:01:15 +0200

    OWS layer download

--- ogc/models/layer.py
+++ ogc/models/layer.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from ogc.models.server import Server
+import os
+from tempfile import TemporaryDirectory
+import zipfile
+
+import fiona
+from fiona.collection import BytesCollection
+import rasterio as rio
+from rasterio.io import MemoryFile
+from io import BytesIO
         
 class Layer(models.Model):
     ''' Layer in an OWS server '''
@@ -14,6 +23,11 @@ class Layer(models.Model):
     tiled = models.BooleanField(_('tiled'), default=True)
     tiled.Boolean=True
     legend = models.URLField(_('legend_url'), null=True, blank=True)
+
+            
+    class Meta:
+        verbose_name = _('Layer')
+
 
     def _update_legend(self, style='default'):
         url = self.details().styles[style]['legend']
@@ -31,7 +45,6 @@ class Layer(models.Model):
         
     def legend_url(self, style='default'):
         return self._try_update_legend(style) if self.legend is None else self.legend
-    # ---------------------------------------------------------------------------
     
     def __str__(self):
         return '{}:{}'.format(self.server, self.layername)
@@ -55,6 +68,95 @@ class Layer(models.Model):
             return self.set_extent()
         else:
             return list(map(float,self.bbox.split(',')))
+
+    def download(self, **options):
+        '''
+        download layer from server
+        options:
+        - srs: srs of output. default=32737 (WGS84/UTM 37N)
+        - bbox: bounding box in srs units
+        - width: output width in pixels
+        - height: output width in pixels
+        - scale desired output scale in srs units. Note that scale overrides width/height parameters 
+        '''
+        service = self.server.service
+    
+        # TODO: implement bbox and custom srs (defaults to WGS84/UTM 37N)
+        if self.server.service_type == 'WMS':
+            # TODO: implement custom scale
             
-    class Meta:
-        verbose_name = _('Layer')
+            details = self.details()
+    #         scale = int(options.get('scale',0)) 
+    #         srs = options.get('srs','32737')
+    #         if srs:
+    #             try:
+    #                 bbox = next(filter(lambda b: b[-1] == srs, details.crs_list))
+    #             except StopIteration:
+    #                 return HttpResponseBadRequest(f'SRS not supported: {srs}') 
+    #         else:
+            # default SRS
+            bbox = details.boundingBox
+            srs = bbox[-1]
+            
+            size = (bbox[2]-bbox[0], abs(bbox[3]-bbox[1]))
+            height = int(options.get('height',0))
+            width = int(options.get('width',0))
+            if height == 0 and width == 0:
+                # default size is 400 pix high and auto width
+                height = 400
+            if width == 0:
+                width = int((height * size[0]) / size[1])
+            elif height == 0:
+                height = int((width * size[1]) / size[0])
+                
+            image = service.getmap(layers = [self.layername], 
+                                bbox = bbox[:-1],
+                                format = 'image/png',
+                                srs = srs,
+                                size = (width, height),
+                                transparent = True
+                                )
+            # QGIS server does not support geoTIFF output
+            # user rasterio to convert PNG output to geoTIFF
+            with rio.open(image) as src:
+                with MemoryFile() as tif:
+                    with tif.open(driver='GTiff', 
+                                  dtype='uint8', 
+                                  width=width, 
+                                  height=height, 
+                                  count=4,
+                                  crs=srs,
+    #                               compress='JPEG',
+    #                               photometric='YCBCR',
+                                  transform=rio.Affine(size[0]/width, 0, bbox[0], 0, -size[1]/height, bbox[3])
+                                  ) as dest:
+                        dest.write(src.read())
+                    return (f'{self.layername}.tif', tif.read(), 'image/tiff')
+        
+        elif self.server.service_type == 'WFS':
+            
+            def zipdir(folder):
+                io = BytesIO()
+                with zipfile.ZipFile(io, mode="w",compression=zipfile.ZIP_DEFLATED) as zf:
+                    for root, _dirs, files in os.walk(folder):
+                        for fname in files:
+                            zf.write(os.path.join(root, fname), fname)
+                return io.getvalue()
+            
+    
+            features = service.getfeature(typename=self.layername, outputFormat='GeoJSON')
+            # QGIS server does not support ESRI shapefile output
+            # Use fiona to convert geojson to shp
+            with BytesCollection(features.read()) as source:
+                # save features as shapefile in temp dir, return zipped dir as attachment in response
+                with TemporaryDirectory() as tempdir:
+                    with fiona.open(os.path.join(tempdir,f'{self.layername}.shp'), 
+                                    mode = 'w', 
+                                    driver='ESRI Shapefile',
+                                    crs=source.crs,
+                                    schema=source.schema) as sink:
+                        for feature in source:
+                            sink.write(feature)
+                            
+                    return (f'{self.layername}.zip', zipdir(tempdir), 'application/zip')
+        
--- ogc/views.py
+++ ogc/views.py
@@ -1,26 +1,12 @@
 import json
-import os
 
-from django.http.response import JsonResponse, HttpResponse, \
-    HttpResponseBadRequest
+from django.http.response import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 from owslib.feature.schema import get_schema
 
 import geopandas as gpd
 from ogc.models.layer import Layer
 from ogc.models.server import Server
-
-
-def zipdir(folder):            
-    from io import BytesIO
-    import zipfile
-    io = BytesIO()
-
-    with zipfile.ZipFile(io, mode="w",compression=zipfile.ZIP_DEFLATED) as zf:
-        for root, _dirs, files in os.walk(folder):
-            for fname in files:
-                zf.write(os.path.join(root, fname), fname)
-    return io.getvalue()
 
 
 def properties1(request, pk):
@@ -50,6 +36,7 @@ def properties(request, pk):
             'properties': {key:str(val) for key, val in features.dtypes.items()}
         }})
 
+
 def statistics(request, pk):
     ''' return statistics of layer's properties '''
     layer = get_object_or_404(Layer, pk=pk)
@@ -61,6 +48,7 @@ def statistics(request, pk):
     # pandas cannot describe geometries
     result = table.drop('geometry',axis=1).describe(include='all').to_json()
     return HttpResponse(result,content_type='application/json')
+
 
 def layers(request, pk):
     ''' return server's layer list with properties '''
@@ -81,6 +69,7 @@ def layers(request, pk):
             'version': server.version,
             'layers': layers
         }})
+
 
 def legends(request, pk):
     ''' return layer's legends '''
@@ -103,85 +92,19 @@ def legends(request, pk):
     legends = [to_json(legend) for legend in layer.legends.all()]
     return JsonResponse({'layer': {'id': layer.id, 'name': layer.layername}, 'legends': legends})
 
+
 def download(request, pk):
     '''
     download layer from server
+    query parameters (all optional):
+    - srs: srs of output. default=32737 (WGS84/UTM 37N)
+    - bbox: bounding box in srs units
+    - width: output width in pixels
+    - height: output width in pixels
+    - scale: desired output scale in srs units. Note that scale overrides width/height parameters 
     '''
-    layer = get_object_or_404(Layer, pk=pk)
-    service = layer.server.service
-
-    if layer.server.service_type == 'WMS':
-        import rasterio as rio
-        from rasterio.io import MemoryFile
-        
-        details = layer.details()
-        srs = request.GET.get('srs')
-        if srs:
-            try:
-                bbox = next(filter(lambda b: b[-1] == srs, details.crs_list))
-            except StopIteration:
-                return HttpResponseBadRequest(f'SRS not supported: {srs}') 
-        else:
-            # default SRS
-            bbox = details.boundingBox
-            srs = bbox[-1]
-        size = (bbox[2]-bbox[0], abs(bbox[3]-bbox[1]))
-        height = int(request.GET.get('height',0))
-        width = int(request.GET.get('width',0))
-        scale = int(request.GET.get('scale',0)) #TODO: implement scale parameter
-        if height == 0 and width == 0:
-            height = 400
-        if width == 0:
-            width = int((height * size[0]) / size[1])
-        elif height == 0:
-            height = int((width * size[1]) / size[0])
-            
-        # QGIS server does not support geoTIFF output
-        # user rasterio to convert PNG output to geoTIFF
-        image = service.getmap(layers = [layer.layername], 
-                            bbox = bbox[:-1],
-                            format = 'image/png',
-                            srs = srs,
-                            size = (width, height),
-                            transparent = True
-                            )
-        with rio.open(image) as src:
-            with MemoryFile() as tif:
-                with tif.open(driver='GTiff', 
-                              dtype='uint8', 
-                              width=width, 
-                              height=height, 
-                              count=4, 
-                              crs=srs,
-#                               compress='JPEG',
-#                               photometric='YCBCR',
-                              transform=rio.Affine(size[0]/width, 0, bbox[0], 0, -size[1]/height, bbox[3])
-                              ) as dest:
-                    dest.write(src.read())
-                response = HttpResponse(tif.read(), content_type='image/tif')
-                response['Content-Disposition'] = f'attachment; filename={layer.layername}.tif'
-                return response
-    
-    elif layer.server.service_type == 'WFS':
-        import fiona
-        from fiona.collection import BytesCollection
-        from tempfile import TemporaryDirectory
-
-        # QGIS server does not support ESRI shapefile output
-        # Use fiona to convert geojson to shp
-        features = service.getfeature(typename=layer.layername, outputFormat='geoJSON')
-        with BytesCollection(features.read()) as source:
-            # save features as shapefile in temp dir, return zipped dir as attachment
-            with TemporaryDirectory() as tempdir:
-                with fiona.open(os.path.join(tempdir,f'{layer.layername}.shp'), 
-                                mode = 'w', 
-                                driver='ESRI Shapefile',
-                                crs=source.crs,
-                                schema=source.schema) as sink:
-                    for feature in source:
-                        sink.write(feature)
-                        
-                response = HttpResponse(zipdir(tempdir), content_type='application/zip')
-                response['Content-Disposition'] = f'attachment; filename={layer.layername}.zip'
-                return response
-                
+    layer = get_object_or_404(Layer,pk=pk)
+    filename, content, content_type = layer.download(**request.GET)
+    response = HttpResponse(content, content_type=content_type)
+    response['Content-Disposition'] = f'attachment; filename={filename}'
+    return response
